e8e9a4849b0167d38b86b06e2540b06e7f69ca10
kishi <trsasasusu@gmail.com>
2016-09-28 01:04:54 +0900

    Deal with some difference among htmls.

--- bond_html.py
+++ bond_html.py
@@ -20,23 +20,45 @@ if __name__ == '__main__':
             break
         output_txt += target_txt[i:index]
 
-        index_of_end_of_filename = target_txt.find('\")', index)
+        index_of_end_of_filename = target_txt.find('\"', index + 12)
         if index_of_end_of_filename == -1:
             print("Error! There is not \".")
             sys.exit()
             break
 
         filename = target_txt[index + 12:index_of_end_of_filename]
-#        for i in range(index_of_end_of_filename - index - 1):
-#            filename += target_txt[i + index + 1]
 
         bonded_file = open(filename, 'r', encoding='utf-8')
         bonded_text = bonded_file.read()
         bonded_file.close()
 
-        output_txt += bonded_text
+        if target_txt[index_of_end_of_filename + 1] == ')':
+            output_txt += bonded_text
 
-        i = index_of_end_of_filename + 2
+            i = index_of_end_of_filename + 2
+        else:
+            change_row_num = ''
+            j = 0
+            while True:
+                if target_txt[index_of_end_of_filename + 2 + j] == ',':
+                    break
+                if target_txt[index_of_end_of_filename + 2 + j] == ' ':
+                    j += 1
+                    continue
+                change_row_num += target_txt[index_of_end_of_filename + 2 + j]
+                j += 1
+
+            index_of_first_of_replacetext = target_txt.find('\"', index_of_end_of_filename + 2)
+            index_of_end_of_replacetext = target_txt.find('\")', index_of_end_of_filename + 2)
+            if index_of_end_of_replacetext == -1:
+                print("Error!")
+                sys.exit()
+
+            splited_bonded_text = bonded_text.split('\n')
+            splited_bonded_text[int(change_row_num) - 1] = target_txt[index_of_first_of_replacetext + 1:index_of_end_of_replacetext]
+            output_txt += '\n'.join(splited_bonded_text)
+
+            i = index_of_end_of_replacetext + 2
 
     output_file = open(sys.argv[3], "w", encoding='utf-8')
     output_file.write(output_txt)
